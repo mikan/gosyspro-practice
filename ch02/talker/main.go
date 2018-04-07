108c45f75934605b04acf5e72246ab59cba293f2
mikan <yutakakato@icloud.com>
2018-04-07 13:37:52 +0900

    add comments

--- ch02/talker/main.go
+++ ch02/talker/main.go
@@ -2,30 +2,33 @@ package main
 
 import "fmt"
 
+// Talker interface
 type Talker interface {
 	Talk()
 }
 
+// Greeter struct
 type Greeter struct {
 	name string
 }
 
+// Talk prints name with message
 func (g Greeter) Talk() {
 	fmt.Printf("Hello, my name is %s\n", g.name)
+	g.name = "updated"
 }
 
 func main() {
 	var talker Talker
 	talker = &Greeter{"wozozo"}
-	talker.Talk()
+	talker.Talk() // Hello, my name is wozozo
 
 	g := &Greeter{"wozozo"}
-	g.Talk()
+	g.Talk() // Hello, my name is wozozo
 	g.name = "foo"
-	g.Talk()
+	g.Talk() // Hello, my name is foo
 
 	g2 := Greeter{"wozozo"}
-	g2.Talk()
-	g2.name = "bar"
-	g2.Talk()
+	g2.Talk() // Hello, my name is wozozo
+	g2.Talk() // Hello, my name is wozozo
 }

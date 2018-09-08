b58e2c9bc8f4bd5a2d10fc3cb1193b29118aa6ce
mikan <yutakakato@icloud.com>
2018-09-08 15:03:03 +0900

    add ch13

---
+++ ch13/gr2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tasks := []string{
+		"cmake ..",
+		"cmake . --build Release",
+		"cpack",
+	}
+	for _, task := range tasks {
+		go func() {
+			fmt.Println(task)
+		}()
+	}
+	time.Sleep(time.Second)
+}
+// Result:
+// cpack
+// cpack
+// cpack
